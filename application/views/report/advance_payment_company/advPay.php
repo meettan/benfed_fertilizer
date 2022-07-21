ccf537b5e7bb2c515f53b4783fb930a450714caa
Suman Mitra <sumanmitra0096@gmail.com>
2022-07-21 18:10:42 +0530

    first commit

---
+++ application/views/report/advance_payment_company/advPay.php
@@ -0,0 +1,192 @@
+<style>
+table {
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid #dddddd;
+
+    padding: 6px;
+
+    font-size: 14px;
+}
+
+th {
+
+    text-align: center;
+
+}
+
+tr:hover {background-color: #f5f5f5;}
+
+</style>
+
+<script>
+  function printDiv() {
+
+        var divToPrint = document.getElementById('divToPrint');
+
+        var WindowObject = window.open('', 'Print-Window');
+        WindowObject.document.open();
+        WindowObject.document.writeln('<!DOCTYPE html>');
+        WindowObject.document.writeln('<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title></title><style type="text/css">');
+
+
+        WindowObject.document.writeln('@media print { .center { text-align: center;}' +
+            '                                         .inline { display: inline; }' +
+            '                                         .underline { text-decoration: underline; }' +
+            '                                         .left { margin-left: 315px;} ' +
+            '                                         .right { margin-right: 375px; display: inline; }' +
+            '                                          table { border-collapse: collapse; font-size: 12px;}' +
+            '                                          th, td { border: 1px solid black; border-collapse: collapse; padding: 6px;}' +
+            '                                           th, td { }' +
+            '                                         .border { border: 1px solid black; } ' +
+            '                                         .bottom { bottom: 5px; width: 100%; position: fixed ' +
+            '                                       ' +
+            '                                   } } </style>');
+        WindowObject.document.writeln('</head><body onload="window.print()">');
+        WindowObject.document.writeln(divToPrint.innerHTML);
+        WindowObject.document.writeln('</body></html>');
+        WindowObject.document.close();
+        setTimeout(function () {
+            WindowObject.close();
+        }, 10);
+
+  }
+</script>
+
+        <div class="wraper"> 
+
+            <div class="col-lg-12 container contant-wraper">
+                
+                <div id="divToPrint">
+
+                    <div style="text-align:center;">
+
+                        <h2>THE WEST BENGAL STATE CO.OP.MARKETING FEDERATION LTD.</h2>
+                        <h4>HEAD OFFICE: SOUTHEND CONCLAVE, 3RD FLOOR, 1582 RAJDANGA MAIN ROAD, KOLKATA-700107.</h4>
+                        <h4>Company Payment Statement Between:<?php echo  date("d/m/Y", strtotime($fDate)).' To '.date("d/m/Y", strtotime($tDate)) ?></h4>
+    
+                        <h5 style="text-align:left"><label>Company: </label> <?php echo $companyName; ?></h5> 
+                     <!--<h5 style="text-align:left"><label>Product:</label> <?php //echo $product->PROD_DESC; ?></h5>-->
+
+                    </div>
+                    <br>  
+
+                    <table style="width: 100%;" id="example">
+
+                        <thead>
+
+                            <tr>
+                            
+                                <th>Sl No.</th>
+                                <th>Date</th>
+                                <th>District</th>
+                                <th>Purchase Invoice</th>
+								<th>FO Name</th>
+                                <th>Product</th>
+                                <th>Purchase Ro</th>
+                                <th>Quantity</th>
+                                <th>Rate</th>
+                                <th>Amount</th>
+                                <th>TDS</th>
+                                <th>NET Amount</th>
+
+                            </tr>
+
+                        </thead>
+
+                        <tbody>
+
+                            <?php
+                            
+
+                                if($tableData){ 
+
+                                    $i = 1;
+                                  $totalRate=0;
+                                  $totalAmount=0;
+                                  $totalTDS=0;
+                                  $totalNETAmount=0;
+                                    foreach($tableData as $ptableData){
+                                       // $total=($ptableData->adv_amt+$total);
+                                       //$total +=$ptableData->adv_amt;
+                            ?>
+<!-- a.pay_dt,c.district_name,a.pur_inv_no,b.PROD_DESC,a.pur_ro,a.qty,a.rate_amt,a.taxable_amt,a.tds_amt,a.net_amt -->
+                                <tr>
+                                     <td><?php echo $i++; ?></td>
+                                     <td><?php echo date("d/m/Y", strtotime($ptableData->pay_dt)); ?></td>
+                                     <td><?php echo $ptableData->br_dist; ?></td>
+                                     <td><?php echo $ptableData->pur_inv_no; ?></td>
+                                     <td><?php echo $ptableData->fo_nm; ?></td>
+									
+                                     <td><?php echo $ptableData->PROD_DESC; ?></td>
+                                     <!-- <td><?= $ptableData->PROD_DESC; ?></td> -->
+                                     <td><?php echo $ptableData->pur_ro; ?></td>
+                                     <td><?php echo $ptableData->qty; ?></td>
+                                     <td><?php echo  round($ptableData->rate_amt,2) ; $totalRate+=$ptableData->rate_amt; ?></td>
+                                     <td><?php echo round($ptableData->taxable_amt,2) ;$totalAmount+=$ptableData->taxable_amt; ?></td>
+                                     <td><?php echo round($ptableData->tds_amt,2);$totalTDS+=$ptableData->tds_amt;?></td>
+                                     <td><?php echo round($ptableData->net_amt,2);$totalNETAmount+=$ptableData->net_amt;?></td>
+                                </tr>
+                               
+ 
+                                <?php    } ?>
+
+                                <tr>
+                                    <td colspan="8"><b>Total</b></td>
+                                    <td><b><?php //echo round($totalRate,2); ?></b></td>
+                                    <td><b><?php echo round($totalAmount,2); ?></b></td>
+                                    <td><b><?php echo round($totalTDS,2); ?></b></td>
+                                    <td><b><?php echo round($totalNETAmount,2); ?></b></td>
+                                </tr>
+                                <?php 
+                                       }
+                                else{
+
+                                    echo "<tr><td colspan='14' style='text-align:center;'>No Data Found</td></tr>";
+
+                                }   
+
+                            ?>
+
+                        </tbody>
+
+                    </table>
+					<table style="margin-top: 100px; border:none;" id="example" width="100%" cellspacing="0" cellpadding="0" border="0">
+				<tbody style="border:none;">
+					<tr style="border:none;">
+					 
+					  <td style="border:none;">Prepared By</td>
+					 
+					  						<td style="border:none;">Asst Manager/Dy. Manager</td>
+					  <td style="border:none;">Departmental Manager(S)</td>
+					  <td style="border:none;">CA&amp;AO</td>
+					  <td style="border:none;">General Manager</td>
+					  					</tr>
+				</tbody>
+						
+						</table>
+
+                </div>   
+                
+                <div style="text-align: center;">
+
+                    <button class="btn btn-primary" type="button" onclick="printDiv();">Print</button>
+                   <!-- <button class="btn btn-primary" type="button" id="btnExport" >Excel</button>-->
+
+                </div>
+
+            </div>
+            
+        </div>
+        
+    <script type="text/javascript">
+        /*$(function () {
+            $("#btnExport").click(function () {
+                $("#example").table2excel({
+                    filename: "Cheque status for <?php echo get_district_name($this->input->post("branch_id")) ?> branch for paddy procurement between Block Societywise Paddy Procurement Between <?php echo date("d-m-Y", strtotime($this->input->post('from_date'))).' To '.date("d-m-Y", strtotime($this->input->post('to_date')));?>.xls"
+                });
+            });
+        });*/
+    </script>
---
+++ application/views/report/cust_payblepaid/stk_stmt_ip.php
@@ -0,0 +1,306 @@
+<style>
+table {
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid #dddddd;
+
+    padding: 6px;
+
+    font-size: 14px;
+}
+
+th {
+
+    text-align: center;
+
+}
+
+tr:hover {background-color: #f5f5f5;}
+
+.form-wraper {
+    margin-bottom: 20px !important;
+}
+
+</style>
+
+
+    
+    <div class="wraper">  
+        <?php //print_r($this->session->userdata('loggedin')); ?>
+        <div class="col-md-12 container form-wraper">
+    
+                 <form method="POST" id="form" action="<?php echo site_url("fert/rep/cust_payblepaid");?>" >
+
+                <div class="form-header">
+                
+                    <h4>Inputs</h4>
+                
+                </div>
+
+                <div class="form-group row">
+
+                    <label for="from_dt" class="col-sm-2 col-form-label">From Date:</label>
+
+                    <div class="col-sm-6">
+
+                        <input type="date"
+                               name="from_date"
+                               class="form-control required"
+                               min='<?php echo explode('-',$this->session->userdata('loggedin')['fin_yr'])[0].'-04-01' ?>'
+                               max='<?php echo ((explode('-',$this->session->userdata('loggedin')['fin_yr'])[0])+1).'-03-31' ?>'
+                               value="<?= explode('-',$this->session->userdata('loggedin')['fin_yr'])[0].'-04-01';//$frm_dt;?>"
+                               readonly
+                        />  
+
+                    </div>
+
+                </div>
+
+                <div class="form-group row">
+
+                    <label for="to_date" class="col-sm-2 col-form-label">To Date:</label>
+
+                    <div class="col-sm-6">
+
+                        <input type="date"
+                               name="to_date"
+                               class="form-control required"
+                               min='<?php echo explode('-',$this->session->userdata('loggedin')['fin_yr'])[0].'-04-01' ?>'
+                               max='<?php echo ((explode('-',$this->session->userdata('loggedin')['fin_yr'])[0])+1).'-03-31' ?>'
+                               value="<?= $to_dt;?>"
+                        />  
+
+                    </div>
+
+                </div>  
+
+
+                <div class="form-group row">
+
+                    <div class="col-sm-10">
+
+                        <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
+
+                    </div>
+
+                </div>
+
+            </form>    
+
+        </div>
+
+    <!-- </div> -->
+    <?php if(isset($_POST["submit"])){ ?>
+        <?php //echo '<pre>'; print_r($all_data); echo '</pre>';?>
+ <!-- <div class="wraper">  -->
+
+            <div class="col-lg-12 contant-wraper">
+                
+                <div id="divToPrint">
+
+                    <div style="text-align:center;">
+
+                        <h2>THE WEST BENGAL STATE CO.OP.MARKETING FEDERATION LTD.</h2>
+                        <h4>HEAD OFFICE: SOUTHEND CONCLAVE, 3RD FLOOR, 1582 RAJDANGA MAIN ROAD, KOLKATA-700107.</h4>
+                        <h4>Societywise Due Register Between: <?php echo $_SESSION['date']; ?></h4>
+                        <h5 style="text-align:left"><label>District: </label> <?php echo $br_name->district_name; ?></h5>
+
+                    </div>
+                    <br>  
+
+                    <table style="width: 100%;" id="example">
+
+                        <thead>
+                        <tr>
+                            
+                            <th>Sl No.</th>
+
+                            <th>Society</th>
+
+                            <th>Opening</th>
+                            <th>Advance Deposited</th>
+                            <th>Sale Amount</th>
+                            <th>Advance Adjustment</th>
+                            <th>Credit Note</th>
+                            <th>Other Adjustment</th>
+
+                            <th>Due Amount</th>
+                            <!-- (Opening + Adjustment Deposited) - (Sale + Adjustment + Credit Note + Others Adjustment) -->
+
+                        </tr>
+
+                            <!-- <tr>
+                            
+                                <th>Sl No.</th>
+
+                                <th>Society</th>
+
+                                <th>Opening</th>
+								<th>Sale Amount</th>
+
+                                <th>Received Amount( Including <br>Advance 
+								& Credit Note Adjustment )</th>
+
+                                <th>Advance Deposit</th>
+
+                                <th>Credit Note</th>
+
+                                
+                             
+                                <th>Due Amount</th>
+
+                            </tr> -->
+
+                        </thead>
+
+                        <tbody>
+
+                            <?php
+
+                                if($product){ 
+
+                                    $i = 1;
+                                    $total = 0.00;
+                                    $tot_sale = 0.00;
+                                    $tot_pur  = 0.00;
+									$tot_cr  =0.00;
+									$tot_adv =0.00;
+                                    $val =0;
+
+                                        foreach($all_data as $prodtls){
+                            ?>
+
+                                <tr class="rep">
+                                     <td class="report"><?php echo $i++; ?></td>
+                                     <td class="report"><?php echo $prodtls->soc_name; ?>
+                                     
+                                     <td class="report opening" id="opening">
+                                       <?php  if($prodtls->op_bln < 0){echo abs($prodtls->op_bln).' Cr';}else{echo abs($prodtls->op_bln).' Dr';} ?>
+                                     </td>
+									  <td class="report purchase" id="purchase">
+                                     <?php echo $prodtls->adv;?>
+                                     </td>
+									  <td class="report sale" id="sale">
+                                     <?php echo $prodtls->tot_sale; ?>
+                                       
+                                     </td>
+                                     <td class="report advance" id="advance">
+                                     <?php echo $prodtls->adv_adj; ?>
+                                     
+                                     </td>
+                                     <td class="report cramt" id="cramt">
+                                     <?php echo $prodtls->cramt_adj; ?>
+                                     
+                                     </td>
+                                     <td class="report closing" id="closing">0.00
+                                     <?php //echo number_format($prodtls->tot_sale - $prodtls->adv_dep,2) ;
+                                        // $total += $prodtls->tot_sale - $prodtls->adv_dep ; ?>  
+                                     </td>
+                                     <td>
+                                         <!-- $dueAmount=($prodtls->op_bln + $prodtls->adv) - ($prodtls->tot_sale + Adjustment + $prodtls->cramt_adj + Others Adjustment) ; -->
+                                        <?php 
+
+$op_adv=(-1*$prodtls->op_bln)+(($prodtls->adv))-($prodtls->tot_sale + $prodtls->adv_adj);
+//-($prodtls->tot_sale)-($prodtls->adv_adj)-($prodtls->cramt_adj);
+// $advAdj_totSale_crnAdj=;
+// $closihg=$op_adv-$advAdj_totSale_crnAdj;
+
+
+if($op_adv>0){
+        echo abs($op_adv).'Cr';
+}else{
+    echo abs($op_adv).'Dr';
+}
+                                        // $dueAmount=($prodtls->op_bln + $prodtls->adv) - ($prodtls->adv_adj  + $prodtls->cramt_adj + 0) ;
+                                        //     if($dueAmount<0){
+                                        //         echo abs($dueAmount).'Cr';
+                                        //     }else{
+                                        //         echo abs($dueAmount).'Dr';
+                                        //     }
+                                        ?>
+                                    </td>
+                                   
+                                </tr>
+ 
+                                <?php  
+
+
+                                                        
+                                    }
+                                ?>
+
+ 
+                                <?php 
+                                       }
+                                else{
+
+                                    echo "<tr><td colspan='14' style='text-align:center;'>No Data Found</td></tr>";
+
+                                }   
+
+                            ?>
+
+                        </tbody>
+                        <tfooter>
+                            <tr>
+                               <td class="report" colspan="3" style="text-align:right">Total</td> 
+                               <td class="report"><?=$tot_pur?></td>
+                               <td class="report"><?=$tot_sale?></td>
+                               <!-- <td class="report"></td>  -->
+								<td></td>
+								<td></td>
+                                <td class="report"><?=$total?></td>  
+ 
+                            </tr>
+                        </tfooter>
+                    </table>
+
+                </div>   
+                
+                <div style="text-align: center;">
+
+                    <button class="btn btn-primary" type="button" onclick="printDiv();">Print</button>
+                   <!-- <button class="btn btn-primary" type="button" id="btnExport" >Excel</button>-->
+
+                </div>
+
+            </div>
+            <?php } ?>
+            
+        </div>
+
+        <script>
+  function printDiv() {
+
+        var divToPrint = document.getElementById('divToPrint');
+
+        var WindowObject = window.open('', 'Print-Window');
+        WindowObject.document.open();
+        WindowObject.document.writeln('<!DOCTYPE html>');
+        WindowObject.document.writeln('<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title></title><style type="text/css">');
+
+
+        WindowObject.document.writeln('@media print { .center { text-align: center;}' +
+            '                                         .inline { display: inline; }' +
+            '                                         .underline { text-decoration: underline; }' +
+            '                                         .left { margin-left: 315px;} ' +
+            '                                         .right { margin-right: 375px; display: inline; }' +
+            '                                          table { border-collapse: collapse; font-size: 12px;}' +
+            '                                          th, td { border: 1px solid black; border-collapse: collapse; padding: 6px;}' +
+            '                                           th, td { }' +
+            '                                         .border { border: 1px solid black; } ' +
+            '                                         .bottom { bottom: 5px; width: 100%; position: fixed ' +
+            '                                       ' +
+            '                                   } } </style>');
+        WindowObject.document.writeln('</head><body onload="window.print()">');
+        WindowObject.document.writeln(divToPrint.innerHTML);
+        WindowObject.document.writeln('</body></html>');
+        WindowObject.document.close();
+        setTimeout(function () {
+            WindowObject.close();
+        }, 10);
+
+  }
+</script>
