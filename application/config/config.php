10ae11b2f6ec49f7638b29a35ef255f0408d0ac8
Tan <meettan@gmail.com>
2022-10-14 16:04:59 +0530

    default timezone in ist

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
  
+date_default_timezone_set('asia/kolkata');
 
-$config['base_url'] = 'http://localhost/benfed/benfed_fertilizer/';
+$config['base_url'] = 'https://benfed.in/benfed_fertilizer/';
 // $config['base_url'] = 'http://192.168.1.102:8080/benfed_fertilizer/';
 
 /*
