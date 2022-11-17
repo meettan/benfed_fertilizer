e24f21993efcf1ea5926bd71c3edecd1a4ee3ef8
meettan <meettan@synergicsoftek.com>
2022-11-17 13:06:13 +0530

    17112022

--- application/controllers/fertilizer/Advance.php
+++ application/controllers/fertilizer/Advance.php
@@ -373,7 +373,8 @@ public function advancefilter(){
         "a.soc_id=b.soc_id"   => NULL,
         // "a.receipt_no=c.receipt_no"   => NULL,
 
-        "b.district"            => $this->session->userdata['loggedin']['branch_id'],
+        //"b.district"            => $this->session->userdata['loggedin']['branch_id'],
+    	 "a.branch_id"            => $this->session->userdata['loggedin']['branch_id'],
 
         "a.fin_yr"              => $this->session->userdata['loggedin']['fin_id'],
 		"a.trans_type='I'"   => NULL,
@@ -386,7 +387,7 @@ public function advancefilter(){
 	$adv['data']    = $this->AdvanceModel->f_select("tdf_advance a,mm_ferti_soc b",$select,$where,0);
 	// echo $this->db->last_query();
 	// exit();
-	$adv['data']    = $this->AdvanceModel->f_select("tdf_advance a,mm_ferti_soc b",$select,$where,0);
+	// $adv['data']    = $this->AdvanceModel->f_select("tdf_advance a,mm_ferti_soc b",$select,$where,0);
 
 	$this->load->view("post_login/fertilizer_main");
 
