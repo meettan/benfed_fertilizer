5ecfdb7f251949e1e5ef1f2d9d041b198abd069e
meettan <meettan@synergicsoftek.com>
2023-01-16 15:36:54 +0530

    change Company Payment

--- application/views/post_login/fertilizer_home_one.php
+++ application/views/post_login/fertilizer_home_one.php
@@ -337,7 +337,7 @@
             <div class="col-sm-12">
               <h2 class="onClickOpen" data-toggle="collapse" data-target="#demo">
                 Company Payment <span>(Click to Expand)</span>
-                <!-- <i class="fa fa-arrow-circle-down" aria-hidden="false"></i> -->
+                 <i class="fa fa-arrow-circle-down" aria-hidden="false"></i> 
               </h2>
             </div>
 
