ad4f6dc4ecf0561b9580b3b4da2fc2013617b798
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-10-16 15:43:21 +0530

    chnage

--- application/views/report/advance_payment/advPay.php
+++ application/views/report/advance_payment/advPay.php
@@ -260,7 +260,7 @@
                             <td colspan="3"><b>Total</b></td>
                             <td style="text-align: right;"><b><?php echo round($total, 2); ?></b></td>
                             <td style="text-align: right;"><b><?php echo round($totalTds, 2); ?></b></td>
-                            <td style="text-align: right;"><b><?php echo $totalnetamt; $totalnetamt;  ?></b></td>
+                            <td style="text-align: right;"><b><?php echo $totalnetamt;  ?>fdff <?=$summary_tot?></b></td>
                         </tr>
                     <?php
                     } else {
