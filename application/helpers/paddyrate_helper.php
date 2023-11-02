ca02261224da05801c097aafbbdf4f004068bd0e
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-11-02 17:01:03 +0530

    chnage

--- application/helpers/paddyrate_helper.php
+++ application/helpers/paddyrate_helper.php
@@ -106,7 +106,7 @@
       $first_p =  intdiv($decimal_part, 10); 
       $second_p = fmod($decimal_part, 10) ;
      }
-     $wordsp = array(0 => '', 1 => 'one', 2 => 'two',
+     $wordsp = array(0 => 'zero', 1 => 'one', 2 => 'two',
      3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
      7 => 'seven', 8 => 'eight', 9 => 'nine');
      if($decimal_part > 0 ){
