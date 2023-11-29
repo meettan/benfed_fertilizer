a6a5aabe761d50be3f33fc5a0af59a10fdec4f12
lokesh kumar jha <lokesh@synergicsoftek.com>
2023-11-29 17:11:26 +0530

    chnage

--- application/views/report/adv_forward_detail/advPay.php
+++ application/views/report/adv_forward_detail/advPay.php
@@ -159,7 +159,7 @@
                             <tr>
                                 <td><?php echo $i++; ?></td>
                                 <td><?php echo date("d/m/Y", strtotime($ptableData->trans_dt)); ?></td>
-                                <td><?php echo date("Y-m-d H:i:s", strtotime($ptableData->created_at)); ?></td>
+                                <td><?php if($ptableData->created_at !=NULL) echo date("Y-m-d H:i:s", strtotime($ptableData->created_at)); ?></td>
                                 <td><?php echo $ptableData->branch_name; ?></td>
                                 <td><?php echo $ptableData->PROD_DESC; ?></td>
                                 <td><?= $ptableData->qty; ?></td>
@@ -227,108 +227,8 @@
 
 
 <div id="divadvToPrint"   style="display:none">
-            <table style="width:100%;border:none !important;" >
-                <tr>
-                    <td style="width:20%"><img src="<?=base_url()?>benfed.png"></td>
-                    <td style="width:79%"><span class="headertitle">The West Bengal State Co-operative Marketing Federation Ltd.</span>
-                <br><span class="headeraddress">Southend Conclave, 3rd Floor,<br> 1582 Rajdanga Main Road, Kolkata-700107.</span>
         
-                 </td>
-                </tr>
-            </table>
-            <div style="text-align:center;">
-                
-            
-                
-                <?php  $bank_name = '';$branch_name = ''; $acc_num = '';$address ='';
-                 $cbank_name ='';$cbranch_name = '';$cacc_num = '';$cifsc ='';$comp_name='';
-                // if($company_id != 1){
-                    foreach ($tableData as $bnk) {
-                    $bank_name = $bnk->bnk;$branch_name = $bnk->bnk_branch_name; $acc_num = $bnk->ac_no;$comp_name= $bnk->comp_name;
-                    $cbank_name = $bnk->cbank;$cbranch_name = $bnk->cbnk_branch_name; $cacc_num = $bnk->cac_no;$cifsc=$bnk->cifsc;
-                                                        break;
-                                                    }; 
-                //   }                                
-                                                    ?>
-                <p style="text-align:left"> &ensp;SCMF/FIN/&ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp;&ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp;<b>Date:<?=date('d/m/Y')?></b></p>
-                <h5 style="text-align:left;font-size:18px"><label>To</label> &ensp;&ensp;<br>The Manager</br><?=$bank_name?>,<br><?=$branch_name?>,
-                <br>Kolkata - 700019</h5>
-
-            </div>
-            <br>
-           <div style="line-height: 1.6;font-size:18px">
-            Sir,<br>&ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp; &ensp;&ensp;We are authorizing you to remit by debiting our Savings Account No.  <?=$acc_num?> in favour of
-            <?=$comp_name?> through NEFT/RTGS/Fund Transfer, details are being provided below:
-
-           <br><br>
-           <?php if($company_id == 1|| $company_id == 10 || $company_id == 11) { ?>
-           <table style="width:100%;border: 1px solid black !important;border-collapse:collapse !important;" >
-                <thead>
-                    <tr  >
-                        <th style="border: 1px solid black !important">DISTRICT</th>
-                        <th style="border: 1px solid black !important">IFS CODE</th>
-                        <th style="border: 1px solid black !important">BENEACCNO</th>
-                        <!-- <th style="border: 1px solid black !important">BENENAME</th> -->
-                        <th style="border: 1px solid black !important">AMOUNT</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php   $totalnetamt = 0;
-                            if($tableData) {
-                            $i = 1;
-                            $total = 0;
-                            $totalnetamt = 0;
-                            $totalTds = 0;
-                            $tds = 0;
-                            foreach ($tableDatasummary as $ptableDatasummary) {
-                            $total += $ptableDatasummary->adv_amt;
-                    ?>
-                    <tr>
-                        <td style="border: 1px solid black !important"><?php if(!empty($ptableDatasummary->fo_name)){echo $ptableDatasummary->fo_name;}?></td>
-                        <td style="border: 1px solid black !important"><?=$cifsc?></td>
-                        <td style="border: 1px solid black !important"><?php if(!empty($ptableDatasummary->fo_number)){echo $ptableDatasummary->fo_number;}else{ echo "";} ?></td>
-                       
-                        <?php  $tds = round((0.001 * $ptableDatasummary->adv_amt),2); ?>
-                        <td style="border: 1px solid black !important"><?php $netamt = round(($ptableDatasummary->adv_amt - $tds),2);  ?>
-                       
-                            <?php        echo $netamt;
-                                    $totalnetamt = $totalnetamt + $netamt; ?></td>
-                    </tr>
-
-                    <?php    }  }?>
-                    <tr>
-                        <td colspan="3" style="text-align:center">TOTAL</td>
-                        <td><?php echo $summary_tot; ?> </td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" style="text-align:center">Rupee : <?=getIndianCurrency($summary_tot)?></td>
-                    </tr>
-                </tbody>
-               
-            <table> 
-            <?php }else { ?>
-            <table style="width:100%;border:none !important;font-weight:bold" >
-            <tr>
-                    <td style="width:30%">Name of the bank:</td><td><?=$cbank_name?></td>
-                <tr>
-                <tr>
-                    <td style="width:30%">Branch Name:</td><td><?=$cbranch_name?></td>
-                <tr>
-                <!-- <tr>
-                    <td style="width:30%">Name of A/C Holder:</td><td><?=$companyName?></td>
-                <tr> -->
-                <tr>
-                    <td style="width:30%">Account no :</td><td><?=$cacc_num?> </td>
-                <tr>
-                <tr>
-                    <td style="width:30%">IFS CODE :</td><td><?=$cifsc?></td>
-                <tr>
-                <tr>
-                    <td style="width:30%">Amount :</td><td>Rs.  <?php echo $totalnetamt; ?> (<?=getIndianCurrency($totalnetamt)?>)</td>
-                <tr>
-             <table>   
-            <?php } ?> 
-           </div>
+         
 
             <table style="width: 100%; background-color: #D5D5D5;" id="example">
 
