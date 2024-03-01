942eb3d7bfef8a8f6d825ca1c69167a11b447eff
Suman Mitra <sumanmitra0096@gmail.com>
2024-03-01 14:49:57 +0530

    change

--- application/controllers/fertilizer/Purchase.php
+++ application/controllers/fertilizer/Purchase.php
@@ -3913,7 +3913,7 @@ class Purchase extends MX_Controller
 		$br_cd         = $this->session->userdata['loggedin']['branch_id'];
 		$fin_id        = $this->session->userdata['loggedin']['fin_id'];
 		$select = array('a.memo_no','a.qty','b.COMP_NAME','c.PROD_DESC','d.invoice_dt as trans_dt','d.no_of_days');
-		$where = array('a.comp_id = b.COMP_ID'=> NULL,'a.prod_id = c.PROD_ID'=> NULL,'a.memo_no = d.indent_memo_no'=> NULL,'a.branch_id' =>$dist_id,'a.fin_id'=>$fin_id );
+		$where = array('a.comp_id = b.COMP_ID'=> NULL,'b.comp_id = d.COMP_ID'=> NULL,'a.prod_id = c.PROD_ID'=> NULL,'c.prod_id = d.PROD_ID'=> NULL,'a.memo_no = d.indent_memo_no'=> NULL,'a.branch_id' =>$dist_id,'a.fin_id'=>$fin_id );
 		$bank['data']  = $this->PurchaseModel->f_select('td_purchase_ackw a,mm_company_dtls b,mm_product c,td_purchase d',$select,$where,0);
 		
 		$page_data = $this->load->view("purchase_ackw/ackwdtls", $bank, true);
