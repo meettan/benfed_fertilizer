0784058692154feb0010d3b92bc6b29bd8635e8e
Suman Mitra <sumanmitra0096@gmail.com>
2024-05-28 15:51:39 +0530

    change

--- application/controllers/fertilizer/Advance.php
+++ application/controllers/fertilizer/Advance.php
@@ -435,10 +435,11 @@ public function advancefilter(){
 	$this->load->view('post_login/footer');
 	}else{
 
-		$select	=	array("a.trans_dt","a.receipt_no","a.soc_id","a.trans_type","b.soc_name","a.adv_amt","a.forward_flag forward_flag","(SELECT count(*)no_of_rcpt FROM td_adv_details c where a.receipt_no=c.receipt_no)as no_of_rcpt");
+		$select	=	array("a.trans_dt","a.receipt_no","a.soc_id","a.trans_type","c.bank_name","b.soc_name","a.adv_amt","a.forward_flag forward_flag","(SELECT count(*)no_of_rcpt FROM td_adv_details c where a.receipt_no=c.receipt_no)as no_of_rcpt");
 
 	$where  =	array(
         "a.soc_id=b.soc_id"   => NULL,
+		"a.bank=c.sl_no"   => NULL,
         "b.district"            => $this->session->userdata['loggedin']['branch_id'],
 
         "a.fin_yr"              => $this->session->userdata['loggedin']['fin_id'],
@@ -447,7 +448,7 @@ public function advancefilter(){
 		"a.trans_dt between '".date("Y-m-d")."' and '".date("Y-m-d")."'"=> NULL
     );
 
-		$adv['data']    = $this->AdvanceModel->f_select("tdf_advance a,mm_ferti_soc b",$select,$where,0);
+		$adv['data']    = $this->AdvanceModel->f_select("tdf_advance a,mm_ferti_soc b,mm_feri_bank c",$select,$where,0);
 		// print_r($adv['data']);
 		//  echo $this->db->last_query();
 		//  exit();
