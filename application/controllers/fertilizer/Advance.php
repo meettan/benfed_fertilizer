c5a4a5b629d8a70474c8ff38a674550fc6ee7004
Suman Mitra <sumanmitra0096@gmail.com>
2024-07-22 13:37:51 +0530

    change

--- application/controllers/fertilizer/Advance.php
+++ application/controllers/fertilizer/Advance.php
@@ -1510,8 +1510,8 @@ public function f_get_dist_bnk_dtls(){
 			);
 	
 			$adv_acci= $this->FertilizerModel->f_select_insecticide("mm_ferti_soc",array("i_adv_acc",'i_acc_cd'),$where_advi,1);
-			echo $this->db->last_query();
-		die();
+		// 	echo $this->db->last_query();
+		// die();
 
 			$bbranch=$this->input->post('bank_id');
 			if(empty($bbranch)){
