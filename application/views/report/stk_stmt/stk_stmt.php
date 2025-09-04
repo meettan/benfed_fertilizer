0d0991b93fdc9e4cdc6b3ea81ee0c0b144a3d6f6
Suman Mitra <sumanmitra0096@gmail.com>
2025-09-04 12:01:04 +0530

    chnage

--- application/views/report/stk_stmt/stk_stmt.php
+++ application/views/report/stk_stmt/stk_stmt.php
@@ -260,7 +260,8 @@ tr:hover {background-color: #f5f5f5;}
             </div>
             
         </div>
-
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
         <script>
   $(document).ready(function () {
       var table = $('#example').DataTable({
